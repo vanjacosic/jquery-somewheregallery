ba9fcdaa8f0855b76a8069e7eedc4f4727d612ee
Vanja Cosic <vanjacosic@gmail.com>
2013-07-04 18:13:13 +0200

    Added grunt to the project to handle minification and other tasks. Added a minified version of the plugin.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> by Opbeat - <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      build: {
+        src: 'jquery.somewheregallery.js',
+        dest: 'jquery.somewheregallery.min.js'
+      }
+    },
+    jshint: {
+      files: ['gruntfile.js', 'jquery.somewheregallery.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
